c23d3053db6c06a0aa59328f14bc5a63f5e47eb9
huraru <hurarunium@gmail.com>
2026-07-28 13:43:15 +0900

    [update]ソート機能を追加

--- Assets/script/SlotManager.cs
+++ Assets/script/SlotManager.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using TMPro;
+using System;
 using System.Collections.Generic;
 
 public class SlotManager : MonoBehaviour
 {
+    private const int MaxDeckCost = 10;
     public static SlotManager Instance { get; private set; }
 
     [SerializeField] private DeckSlot[] deckSlots;
@@ -10,6 +13,8 @@ public class SlotManager : MonoBehaviour
 
     [SerializeField] private Card cardPrefab;
     [SerializeField] private CardData[] cardDataList;
+
+    [SerializeField] private TMP_Text deckCostText;
 
     private void Awake()
     {
@@ -21,7 +26,7 @@ public class SlotManager : MonoBehaviour
         //ゲーム開始時、インベントリスロットにランダムなカードを配布する
         foreach (var slot in inventorySlots)
         {
-            var data = cardDataList[Random.Range(0, cardDataList.Length)];
+            var data = cardDataList[UnityEngine.Random.Range(0, cardDataList.Length)];
             var card = Instantiate(cardPrefab, slot.transform);
             card.Setup(data);
             (card.transform as RectTransform).anchoredPosition = Vector2.zero;
@@ -38,7 +43,63 @@ public class SlotManager : MonoBehaviour
         }
 
         from.RemoveCard();
+        int deckCost = CountingDeckCost();
+
+        deckCostText.text = $"Cost: {deckCost}";
+        deckCostText.color = deckCost > MaxDeckCost ? Color.red : Color.black;
+
         return true;
+    }
+
+    /// <summary>
+    /// デッキのコストを計算し返り値として戻す
+    /// </summary>
+    private int CountingDeckCost()
+    {
+        var totalCost = 0;
+        foreach (var slot in deckSlots)
+        {
+            if (slot.CurrentCard != null)
+            {
+                totalCost += slot.CurrentCard.Data.cost;
+            }
+        }
+        return totalCost;
+    }
+
+    public void SortInventoryByCost()
+    {
+        SortInventory((a, b) => a.Data.cost.CompareTo(b.Data.cost));
+    }
+
+    public void SortInventoryByHp()
+    {
+        SortInventory((a, b) => a.Data.hp.CompareTo(b.Data.hp));
+    }
+
+    private void SortInventory(Comparison<Card> comparison)
+    {
+        var cards = new List<Card>();
+        foreach (var slot in inventorySlots)
+        {
+            if (slot.CurrentCard != null)
+            {
+                cards.Add(slot.CurrentCard);
+                slot.RemoveCard();
+            }
+        }
+
+        cards.Sort(comparison);
+
+        for (int i = 0; i < cards.Count; i++)
+        {
+            var slot = inventorySlots[i];
+            var card = cards[i];
+
+            slot.TrySetCard(card);
+            card.transform.SetParent(slot.transform);
+            (card.transform as RectTransform).anchoredPosition = Vector2.zero;
+        }
     }
 
     public ISlot GetSlot(Card card)
